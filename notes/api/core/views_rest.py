b9dade3340942fbd49905c7965880667ed0cc277
andrei-velya <roiale@yandex.ru>
2024-10-24 21:34:44 +0500

    Урок 122. Забыл добавить random

--- notes/api/core/views_rest.py
+++ notes/api/core/views_rest.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+import random
 
 # Create your views here.
 
 @api_view(['GET'])
 def clicks(request):
-    return Response({'clicks':100})
+    return Response({'clicks':random.randint(1,100)})
